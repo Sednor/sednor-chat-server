edb93585ae44e311c1a2b564318aa01da1bb4ff7
vladimir-maklyakov <maklyakov@sednor.net>
2018-03-27 19:08:50 +0300

    Configure CORS

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ let router = express.Router();
 
 router.get('/', (req, res, next) => res.send('SEDNOR-CHAT-SERVER'));
 
+router.post('/post-test', (req, res, next) => res.send('CORS'));
+
 module.exports = router;
