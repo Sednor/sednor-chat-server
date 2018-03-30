0df1439ee3c984f2709c82196711170d369057f3
sednor-0 <company@sendor.net>
2018-03-30 14:22:40 +0300

    Add chat.sednor.net to whitelist

--- app/config/index.js
+++ app/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  whitelist: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://sednor-chat-web.herokuapp.com'],
+  whitelist: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://sednor-chat-web.herokuapp.com', 'http://chat.sednor.net'],
   JWT_SECRET: 'sednor',
   apiPrefix: '/api/v1'
 };
