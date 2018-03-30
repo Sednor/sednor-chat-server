828b12908a750ac46acdf833b0ba77985bd8e093
vladimir-maklyakov <maklyakov@sednor.net>
2018-03-30 17:25:43 +0300

    Change auth header const

--- app/config/index.js
+++ app/config/index.js
@@ -2,5 +2,5 @@ module.exports = {
   whitelist: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://sednor-chat-web.herokuapp.com', 'http://chat.sednor.net'],
   JWT_SECRET: 'sednor',
   apiPrefix: '/api/v1',
-  authHeader: 'Authorization'
+  authHeader: 'authorization'
 };
