e07cb7b93f276787256ecbbc83ae8f51ad6cdb1e
vladimir-maklyakov <maklyakov@sednor.net>
2018-03-30 12:49:57 +0300

    Add additional ports

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    whitelist: ['http://localhost:3001', 'https://sednor-chat-web.herokuapp.com']
+    whitelist: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://sednor-chat-web.herokuapp.com']
 };
