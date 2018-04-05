dc0581c7829978401a9f642c715cef52cfa8193c
vladimir-maklyakov <maklyakov@sednor.net>
2018-04-05 17:04:17 +0300

    Change type for 'users' field

--- app/models/Chat.js
+++ app/models/Chat.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 const ChatSchema = new mongoose.Schema({
   name: String,
-  users: Array,
+  users: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   messages: Array
 }, { timestamps: true });
 
